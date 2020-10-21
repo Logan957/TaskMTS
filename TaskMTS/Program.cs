2083dd12496f528d206152b62683ee02bf3fc5c0
Logan957 <roman.zaidulin2015@gmail.com>
2020-10-21 13:15:13 +0300

    First commit

---
+++ TaskMTS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,8,5,4,5,6,9,5};
+            int value = 5;
+            var NewList = list.Where(x => x != value).ToList();
+            foreach (var g in NewList) 
+            {
+                Console.WriteLine(g);
+            }
+        }
+    }
+}
